ec453fbf7b7287bbd7845eadec2d29cc43d74169
Weslley Martins <weslleymartins2003@gmail.com>
2023-06-16 22:23:33 -0300

    replacement and adjust of profile photo

--- script.js
+++ script.js
@@ -8,17 +8,17 @@ function toggleMode() {
   // substituir a imagem
   if (html.classList.contains("light")) {
     // se tiver light mode, adicionar a imagem light
-    img.setAttribute("src", "./assets/avatar-light.png")
+    img.setAttribute("src", "./assets/avatar.png")
     img.setAttribute(
       "alt",
-      "Foto de Mayk Brito sorrindo, usando óculos com lentes escuras e jaqueta preta, barba e fundo azul e rosa."
+      "Foto de Weslley com fundo amarelo, camisa da seleção brasileira e óculos de grau."
     )
   } else {
     //se tiver sem light mode, manter a imagem normal
-    img.setAttribute("src", "./assets/avatar-dark.png")
+    img.setAttribute("src", "./assets/avatar.png")
     img.setAttribute(
       "alt",
-      "Foto de Mayk Brito sorrindo, usando óculos e jaqueta preta, barba e fundo azul e rosa."
+      "Foto de Weslley com fundo amarelo, camisa da seleção brasileira e óculos de grau."
     )
   }
 }
